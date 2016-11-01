053ac368372d071b7b44819ff4909b42c6d1358b
Stavros Bastakis <stavros@webtrails.gr>
2016-11-01 23:09:34 +0200

    Create Profile component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Profile from './components/Profile';
 
 class App extends Component {
   render() {
@@ -9,9 +10,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2># Devstaff meetup - React.js #</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Profile />
       </div>
     );
   }
---
+++ src/components/Profile.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Profile.css';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div className="profile">
+        <div className="profile-info">
+          <a className="profile-avatar" href='https://github.com/devstaff-crete' target="_blank" title='devstaff-crete'>
+            <img src='https://avatars0.githubusercontent.com/u/13062423?v=3&s=200' alt='devstaff-crete'/>
+          </a>
+          <div className="profile-details">
+            <h2><a href='https://github.com/devstaff-crete' title='devstaff-crete' target="_blank">Devstaff</a></h2>
+            <h3>Crete, Greece</h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
