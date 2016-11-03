ab726d7e5db2e56826909921f69b1aae4cc99433
Stavros Bastakis <stavros@webtrails.gr>
2016-11-04 00:07:08 +0200

    Extract ProfileInfo component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import Stats from './Stats';
+import ProfileInfo from './ProfileInfo';
 
 class Profile extends Component {
   render() {
+    const profile = {
+      githubUrl: 'https://github.com/devstaff-crete',
+      username: 'devstaff-crete',
+      imageUrl: 'https://avatars0.githubusercontent.com/u/13062423?v=3&s=200',
+      name: 'Devstaff',
+      location: 'Crete, Greece', 
+    };
+  
     return (
       <div className="profile">
-        <div className="profile-info">
-          <a className="profile-avatar" href='https://github.com/devstaff-crete' target="_blank" title='devstaff-crete'>
-            <img src='https://avatars0.githubusercontent.com/u/13062423?v=3&s=200' alt='devstaff-crete'/>
-          </a>
-          <div className="profile-details">
-            <h2><a href='https://github.com/devstaff-crete' title='devstaff-crete' target="_blank">Devstaff</a></h2>
-            <h3>Crete, Greece</h3>
-          </div>
-        </div>
+        <ProfileInfo profile={profile} />
         <Stats followers={29} repos={15} following={16} />
       </div>
     );
