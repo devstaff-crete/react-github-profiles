7db8b24049eab91e490a514ca95dcc43c2bc211c
Nikolas Vourlakis <nvourlakis@gmail.com>
2016-11-11 17:42:15 +0200

    Set rel attribute to links with target _blank
    Fixes #1

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -3,18 +3,33 @@ import React, { Component, PropTypes } from 'react';
 class ProfileInfo extends Component {
   render() {
     const profile = this.props.profile;
-  
+
     return (
       <div className="profile-info">
-          <a className="profile-avatar" href={profile.githubUrl} target="_blank" title={profile.username}>
-            <img src={profile.imageUrl} alt={profile.username}/>
-          </a>
-  
-          <div className="profile-details">
-            <h2><a href={profile.githubUrl} title={profile.username} target="_blank">{profile.name}</a></h2>
-            <h3>{profile.location || 'Voriza, Crete'}</h3>
-          </div>
+        <a
+          className="profile-avatar"
+          href={profile.githubUrl}
+          target="_blank"
+          rel='noopener noreferrer'
+          title={profile.username}
+        >
+          <img src={profile.imageUrl} alt={profile.username}/>
+        </a>
+
+        <div className="profile-details">
+          <h2>
+            <a
+              href={profile.githubUrl}
+              title={profile.username}
+              target="_blank"
+              rel='noopener noreferrer'
+            >
+              {profile.name}
+            </a>
+          </h2>
+          <h3>{profile.location || 'Voriza, Crete'}</h3>
         </div>
+      </div>
     );
   }
 }
@@ -25,7 +40,7 @@ ProfileInfo.propTypes = {
     username: PropTypes.string,
     imageUrl: PropTypes.string,
     name: PropTypes.string,
-    location: PropTypes.string, 
+    location: PropTypes.string,
   }).isRequired,
 };
 
